02fee63af2fb21a71b5cbdcb1e585ac096b586cc
Joshua Landron <45753071+jlandron@users.noreply.github.com>
2019-11-09 18:21:41 -0800

    changed line endings to linux style

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-g++ Timer.cpp UpdSocket.cpp udphw3.cpp -o hw3
-g++ Timer.cpp UpdSocket.cpp udphw3case4.cpp -o hw3case4
+g++ Timer.cpp UdpSocket.cpp udphw3.cpp -o hw3
+g++ Timer.cpp UdpSocket.cpp udphw3case4.cpp -o hw3case4
 
 echo "Standard tests compiled to hww3"
 echo "Test case 3 compiled to hw3cas4"
