f1313d6d4c9337f50a2c5655c3b025d970c31117
Joshua Landron <45753071+jlandron@users.noreply.github.com>
2019-11-10 12:52:16 -0800

    .

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@ g++ -O3 Timer.cpp UdpSocket.cpp udphw3.cpp -o hw3
 g++ -O3 Timer.cpp UdpSocket.cpp udphw3case4.cpp -o hw3case4
 
 echo "Standard tests compiled to hw3"
-echo "Test case 3 compiled to hw3cas4"
+echo "Test case 4 compiled to hw3cas4"
